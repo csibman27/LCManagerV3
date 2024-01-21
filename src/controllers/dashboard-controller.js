5a79007173798cc10f15d1dc1a22bda7120c554f
tibor molnar <tmolnair@gmail.com>
2024-01-21 15:36:43 +0000

    add login and signup view and corresponding controllers

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      return h.view("main");
+      const servers = await db.serverStore.getAllServers();
+      const viewData = {
+        title: "LCManager Dashboard",
+        servers: servers,
+      };
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addServer: {
+    handler: async function (request, h) {
+      const newServer = {
+        title: request.payload.title,
+      };
+      await db.serverStore.addServer(newServer);
+      return h.redirect("/dashboard");
     },
   },
 };
---
+++ src/controllers/server-controller.js
@@ -0,0 +1,27 @@
+import { db } from "../models/db.js";
+
+export const serverController = {
+  index: {
+    handler: async function (request, h) {
+      const server = await db.serverStore.getServerById(request.params.id);
+      const viewData = {
+        title: "Server",
+        server: server,
+      };
+      return h.view("server-view", viewData);
+    },
+  },
+
+  addMachine: {
+    handler: async function (request, h) {
+      const server = await db.serverStore.getServerById(request.params.id);
+      const newMachine = {
+        title: request.payload.title,
+        description: request.payload.description,
+        // ipv4: Number(request.payload.duration),
+      };
+      await db.trackStore.addMachine(server._id, newMachine);
+      return h.redirect(`/server/${server._id}`);
+    },
+  },
+};
