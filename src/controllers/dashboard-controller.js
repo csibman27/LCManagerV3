db44889f936ad79b38d607c2d49a9c12cc335f0c
tibor molnar <tmolnair@gmail.com>
2024-01-26 21:47:17 +0000

    dashboaard servers list frontend

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,5 +1,7 @@
 import { db } from "../models/db.js";
 
+const newDate = new Date();
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
@@ -18,6 +20,18 @@ export const dashboardController = {
     handler: async function (request, h) {
       const newServer = {
         title: request.payload.title,
+        cab: Number(request.payload.cab),
+        os: request.payload.os,
+        idrac: request.payload.idrac,
+        desc: request.payload.os,
+        backupTo: request.payload.backupTo,
+        monitoredWith: request.payload.monitoredWith,
+        support: request.payload.support,
+        service: request.payload.service,
+        bios: request.payload.bios,
+        firmware: request.payload.firmware,
+        maas: request.payload.maas,
+        date: newDate.toISOString(), // date in ISO 8601 format.
       };
       await db.serverStore.addServer(newServer);
       return h.redirect("/dashboard");
