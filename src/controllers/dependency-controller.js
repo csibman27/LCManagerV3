d59c77d8c36cec4ba66a4cd372d97817021b778f
tibor molnar <tmolnair@gmail.com>
2024-03-07 23:59:09 +0000

    start dependency tree

--- src/controllers/dependency-controller.js
+++ src/controllers/dependency-controller.js
@@ -1,15 +1,21 @@
+import { db } from "../models/db.js";
+
 export const dependencyController = {
   index: {
-    handler: function (request, h) {
+    handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const loggedInUserInitials = loggedInUser.firstName[0] + loggedInUser.lastName[0];
       const date = new Date().getFullYear();
+      const servers = await db.serverStore.getAllServers();
+      const services = await db.serviceStore.getAllServices();
       const company = "[Company name]";
       const viewData = {
         title: "Dependency Tree",
         date,
         company,
         loggedInUserInitials,
+        servers: servers,
+        services,
       };
       return h.view("dependency-view", viewData);
     },
