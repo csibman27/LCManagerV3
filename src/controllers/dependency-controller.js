365dcd233ef74d555da8002464626179c41a39f9
tibor molnar <tmolnair@gmail.com>
2024-03-12 20:38:43 +0000

    dependency tree done

--- src/controllers/dependency-controller.js
+++ src/controllers/dependency-controller.js
@@ -1,4 +1,6 @@
 import { db } from "../models/db.js";
+import { analytics } from "../utils/analytics.js";
+import { serviceJsonStore } from "../models/json/service-json-store.js";
 
 export const dependencyController = {
   index: {
@@ -6,9 +8,18 @@ export const dependencyController = {
       const loggedInUser = request.auth.credentials;
       const loggedInUserInitials = loggedInUser.firstName[0] + loggedInUser.lastName[0];
       const date = new Date().getFullYear();
+      // test of dependencies
+      const b = [];
       const servers = await db.serverStore.getAllServers();
-      const server = await db.serverStore.getServerById(request.params.id);
-      const string = "Tibor"
+      for (let i = 0; i < servers.length; i++) {
+        const a = servers[i]._id;
+        const server = await db.serverStore.getServerById(a);
+        b.push(server);
+      }
+
+      // console.log("Services: " + b);
+      // console.log("Server: " + JSON.stringify(server));
+
       const company = "[Company name]";
       const viewData = {
         title: "Dependency Tree",
@@ -16,8 +27,7 @@ export const dependencyController = {
         company,
         loggedInUserInitials,
         servers: servers,
-        server,
-        string,
+        b,
       };
       return h.view("dependency-view", viewData);
     },
